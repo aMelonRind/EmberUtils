d6cfca60913b6f507f85578db713867e5236f95e
aMelonRind <kong49123@gmail.com>
2024-01-22 14:35:17 +0800

    initial

---
+++ src/main/java/io/github/amelonrind/emberutils/EmberUtils.java
@@ -0,0 +1,16 @@
+package io.github.amelonrind.emberutils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmberUtils implements ModInitializer {
+    public static final String MOD_ID = "emberutils";
+
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+        // nothing here for now
+    }
+
+}
